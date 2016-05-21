b22df422eb3ffb45289297a9221300bcecd5754c
nguyenhunghau <nguyenhunghau.us@gmail.com>
2016-05-21 10:43:23 +0700

    update database for follow-website tool

--- Database/follow_website.sql
+++ Database/follow_website.sql
@@ -14,11 +14,11 @@ create table Url
     Frequent int,
     LinkUrl varchar(1000) CHARACTER SET utf8 COLLATE utf8_general_ci,
     TimeSave date,
-    Pc varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci,
-    Tablet varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci,
-    Mobile varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci,
-    Html varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci,
+    Pc varchar(1000) CHARACTER SET utf8 COLLATE utf8_general_ci,
+    Tablet varchar(1000) CHARACTER SET utf8 COLLATE utf8_general_ci,
+    Mobile varchar(1000) CHARACTER SET utf8 COLLATE utf8_general_ci,
+    Html varchar(1000) CHARACTER SET utf8 COLLATE utf8_general_ci,
 	constraint FK_Url_User foreign key (IdUser) references User(Id)
 );
 
-insert into User (Id, Username,Passwords)  values (1, 'admin','123456');
+insert into User (Id, Username,Passwords)  values (1, 'admin','e10adc3949ba59abbe56e057f20f883e');
